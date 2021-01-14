40e610c6ddcba39bd2dee8038e528a14c5b1228d
Francesco Varani <francovaro@gmail.com>
2021-01-14 14:59:53 +0100

    base schema
    and type fix

--- src/tetris_handler.c
+++ src/tetris_handler.c
@@ -122,7 +122,7 @@ const s_base_piece_prop pieces[e_piece_last] =
 			{
 				.probability	= 10,
 				.color			= COLOR_CYAN,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<1) | (1<<5) | (1<<9) | (1<<13),
 				.max_height 	= 1,
 				.max_height 	= 4,
 				._type 			= e_piece_I
@@ -131,54 +131,54 @@ const s_base_piece_prop pieces[e_piece_last] =
 			{
 				.probability 	= 10,
 				.color			= COLOR_BLUE,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<1) | (1<<2) | (1<<5) | (1<<9),
 				.max_height 	= 2,
 				.max_height 	= 3,
-				._type 			= e_piece_I
+				._type 			= e_piece_J
 			},
 			// e_piece_L
 			{
 				.probability 	= 10,
 				.color			= COLOR_ORANGE,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<1) | (1<<2) | (1<<6) | (1<<10),
 				.max_height 	= 2,
 				.max_height 	= 3,
-				._type 			= e_piece_I
+				._type 			= e_piece_L
 			},
 			// e_piece_O
 			{
 				.probability	 = 10,
 				.color			= COLOR_YELLOW,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<1) | (1<<2) | (1<<5) | (1<<6),
 				.max_height 	= 2,
 				.max_height 	= 2,
-				._type 			= e_piece_I
+				._type 			= e_piece_O
 			},
 			// e_piece_S
 			{
 				.probability 	= 10,
 				.color			= COLOR_GREEN,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<1) | (1<<2) | (1<<4) | (1<<5),
 				.max_height 	= 2,
 				.max_height 	= 3,
-				._type 			= e_piece_I
+				._type 			= e_piece_S
 			},
 			// e_piece_T
 			{
 				.probability 	= 10,
 				.color			= COLOR_PURPLE,
-				.base_schema 	= 0x0000,
+				.base_schema 	= (1<<0) | (1<<1) | (1<<2) | (1<<5),
 				.max_height 	= 2,
 				.max_height 	= 3,
-				._type 			= e_piece_I
+				._type 			= e_piece_T
 			},
 			// e_piece_Z
 			{
 				.probability 	= 10,
 				.color			= COLOR_RED,
-				.base_schema	= 0x0000,
+				.base_schema	= (1<<0) | (1<<1) | (1<<5) | (1<<6),
 				.max_height 	= 2,
 				.max_height 	= 3,
-				._type 			= e_piece_I
+				._type 			= e_piece_Z
 			}
 		};
